817a3820e94e31408a15f308bfb23d4454e69bf9
wistchze <handidwic1225@gmail.com>
2025-05-11 06:14:24 +0700

    Add helper on ReflValue

--- core/src/value.rs
+++ core/src/value.rs
@@ -5,3 +5,15 @@ pub enum ReflValue<'a> {
     Float(f32),
     Bool(bool),
 }
+
+/// A helper to get what type is it
+impl ReflValue<'_> {
+    pub fn kind(&self) -> &'static str {
+        match self {
+            ReflValue::Str(_) => "str",
+            ReflValue::Int(_) => "int",
+            ReflValue::Float(_) => "float",
+            ReflValue::Bool(_) => "bool",
+        }
+    }
+}
